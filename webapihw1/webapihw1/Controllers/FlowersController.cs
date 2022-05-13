c522c2e1e1763334d852eea58a4afcb7535d5752
avichai321 <77723644+avichai321@users.noreply.github.com>
2022-05-13 17:46:40 +0300

    done

---
+++ webapihw1/webapihw1/Controllers/FlowersController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace webapihw1.Controllers
+{
+    [Route("api/[action]")]
+    [ApiController]
+    public class FlowersController : ControllerBase
+    {
+        public string rose()
+        {
+            return "Rose";
+
+        }
+        public string lily()
+        {
+            return "lily";
+
+        }
+        public string tulip()
+        {
+            return "tulip";
+
+        }
+        public string orchid()
+        {
+            return "orchid";
+
+        }
+    }
+}
